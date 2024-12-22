1e9b717fdb487788e0aac77d4c855c6f435ece14
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-12-22 18:11:20 +0700

    update

--- 1.yolo_gpu.py
+++ 1.yolo_gpu.py
@@ -4,35 +4,44 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolo11n.pt")
 
-# Train the model
-if __name__ == '__main__':
-    train_results = model.train(
-        data="coco8_custom.yaml",  # path to dataset YAML
-        epochs=3,  # number of training epochs
-        imgsz=640,  # training image size
-        device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-        workers=2,
-        batch=1
-    )
+# # Train the model
+# if __name__ == '__main__':
+#     train_results = model.train(
+#         data="coco8_custom.yaml",  # path to dataset YAML
+#         epochs=3,  # number of training epochs
+#         imgsz=640,  # training image size
+#         device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+#         workers=2,
+#         batch=1
+#     )
 
 # run detect with GPU
 # results = model("video1.mp4", device=0, stream=True)
-# # results[0].show()
-# for result in results:
-#     # original image
-#     orig_img = result.orig_img
-#     # after predict image
-#     predict_img = result.plot()
-#     if result:
-#         boxes = result.cpu().boxes.numpy()  # Boxes object for bbox outputs
-#         for box in boxes:  # there could be more than one detection
-#             print("class", box.cls)
-#             print("conf", box.conf)
-#             print("xyxy", box.xyxy)
-#
-#     # show original image
-#     # cv2.imshow("predict img", orig_img)
-#
-#     # show image after predict
-#     cv2.imshow("predict img", predict_img)
-#     cv2.waitKey(1)
+
+# run detect with CPU
+results = model("video1.mp4", device="cpu", stream=True)
+
+# results[0].show()
+for result in results:
+    # original image
+    orig_img = result.orig_img
+    # after predict image
+    predict_img = result.plot()
+    if result:
+        # run with GPU
+        # boxes = result.cpu().boxes.numpy()  # Boxes object for bbox outputs
+
+        # run with CPU
+        boxes = result.boxes.numpy()  # Boxes object for bbox outputs
+
+        for box in boxes:  # there could be more than one detection
+            print("class", box.cls)
+            print("conf", box.conf)
+            print("xyxy", box.xyxy)
+
+    # show original image
+    # cv2.imshow("predict img", orig_img)
+
+    # show image after predict
+    cv2.imshow("predict img", predict_img)
+    cv2.waitKey(1)
